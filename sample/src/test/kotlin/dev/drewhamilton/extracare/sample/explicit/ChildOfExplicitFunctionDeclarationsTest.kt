8233a39a5c373212720f377ee938c9753ea1b35b
Drew Hamilton <drew.hamilton.0@gmail.com>
2020-05-02 19:37:56 +0200

    Fix new test

--- sample/src/test/kotlin/dev/drewhamilton/extracare/sample/explicit/ChildOfExplicitFunctionDeclarationsTest.kt
+++ sample/src/test/kotlin/dev/drewhamilton/extracare/sample/explicit/ChildOfExplicitFunctionDeclarationsTest.kt
@@ -2,15 +2,18 @@ package dev.drewhamilton.extracare.sample.explicit
 
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
+import java.util.Objects
 
 class ChildOfExplicitFunctionDeclarationsTest {
 
     private val childOfExplicitFunctionDeclarations = ChildOfExplicitFunctionDeclarations("test")
 
     @Test fun `ExplicitFunctionDeclarations overrides explicit function declarations`() {
-        assertThat(childOfExplicitFunctionDeclarations.toString()).isEqualTo("test")
-        assertThat(childOfExplicitFunctionDeclarations).isEqualTo(true)
+        assertThat(childOfExplicitFunctionDeclarations.toString())
+            .isEqualTo("ChildOfExplicitFunctionDeclarations(string=test)")
+        assertThat(childOfExplicitFunctionDeclarations).isNotEqualTo(true)
         assertThat(childOfExplicitFunctionDeclarations).isNotEqualTo(false)
-        assertThat(childOfExplicitFunctionDeclarations.hashCode()).isEqualTo("test".hashCode())
+        assertThat(childOfExplicitFunctionDeclarations).isEqualTo(ChildOfExplicitFunctionDeclarations("test"))
+        assertThat(childOfExplicitFunctionDeclarations.hashCode()).isEqualTo(Objects.hash("test") - 31)
     }
 }
