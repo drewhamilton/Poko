e979ec9ae721039e0628572781cee6f280c73ee8
Drew Hamilton <drew.hamilton.0@gmail.com>
2020-05-04 16:46:40 +0200

    Ensure shorts and bytes are compiled too

--- extracare-compiler-plugin/src/test/kotlin/dev/drewhamilton/extracare/ExtraCarePluginTest.kt
+++ extracare-compiler-plugin/src/test/kotlin/dev/drewhamilton/extracare/ExtraCarePluginTest.kt
@@ -84,7 +84,9 @@ class ExtraCarePluginTest {
                 val float: Float,
                 val double: Double,
                 val long: Long,
-                val int: Int
+                val int: Int,
+                val short: Short,
+                val byte: Byte
             )
         """.trimIndent()
     }
