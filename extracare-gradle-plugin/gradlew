7c63346ff245c005b72a0e42cc73b986beae2e53
Drew Hamilton <drew.hamilton.0@gmail.com>
2020-07-25 15:37:23 +0200

    Gradle 6.5.1

--- extracare-gradle-plugin/gradlew
+++ extracare-gradle-plugin/gradlew
@@ -82,6 +82,7 @@ esac
 
 CLASSPATH=$APP_HOME/gradle/wrapper/gradle-wrapper.jar
 
+
 # Determine the Java command to use to start the JVM.
 if [ -n "$JAVA_HOME" ] ; then
     if [ -x "$JAVA_HOME/jre/sh/java" ] ; then
@@ -92,14 +93,12 @@ if [ -n "$JAVA_HOME" ] ; then
     fi
     if [ ! -x "$JAVACMD" ] ; then
         die "ERROR: JAVA_HOME is set to an invalid directory: $JAVA_HOME
-
 Please set the JAVA_HOME variable in your environment to match the
 location of your Java installation."
     fi
 else
     JAVACMD="java"
     which java >/dev/null 2>&1 || die "ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
-
 Please set the JAVA_HOME variable in your environment to match the
 location of your Java installation."
 fi
@@ -129,6 +128,7 @@ fi
 if [ "$cygwin" = "true" -o "$msys" = "true" ] ; then
     APP_HOME=`cygpath --path --mixed "$APP_HOME"`
     CLASSPATH=`cygpath --path --mixed "$CLASSPATH"`
+
     JAVACMD=`cygpath --unix "$JAVACMD"`
 
     # We build the pattern for arguments to be converted via cygpath
--- extracare-gradle-plugin/src/main/kotlin/dev/drewhamilton/extracare/gradle/ExtraCareGradlePlugin.kt
+++ extracare-gradle-plugin/src/main/kotlin/dev/drewhamilton/extracare/gradle/ExtraCareGradlePlugin.kt
@@ -1,10 +1,51 @@
-package dev.drewhamilton.extracare.gradle
+package dev.drewhamilton.extracare.gradle.sub
 
-import org.gradle.api.Plugin
+import com.google.auto.service.AutoService
+import dev.drewhamilton.extracare.gradle.ExtraCareGradlePlugin
+import dev.drewhamilton.extracare.gradle.ExtraCarePluginExtension
+import dev.drewhamilton.extracare.info.ArtifactInfo
 import org.gradle.api.Project
+import org.gradle.api.provider.Provider
+import org.gradle.api.tasks.compile.AbstractCompile
+import org.jetbrains.kotlin.gradle.dsl.KotlinCommonOptions
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilerPluginSupportPlugin
+import org.jetbrains.kotlin.gradle.plugin.KotlinGradleSubplugin
+import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
+import org.jetbrains.kotlin.gradle.plugin.SubpluginArtifact
+import org.jetbrains.kotlin.gradle.plugin.SubpluginOption
 
-class ExtraCareGradlePlugin : Plugin<Project> {
-    override fun apply(target: Project) {
-        target.extensions.create("extraCare", ExtraCarePluginExtension::class.java)
+class ExtraCareGradleSubplugin : KotlinCompilerPluginSupportPlugin {
+
+    override fun isApplicable(kotlinCompilation: KotlinCompilation<*>): Boolean =
+        when (kotlinCompilation.platformType) {
+            KotlinPlatformType.jvm, KotlinPlatformType.androidJvm -> true
+            else -> false
+        }
+
+    override fun getCompilerPluginId(): String = ArtifactInfo.COMPILER_PLUGIN_ARTIFACT
+
+    override fun getPluginArtifact() = SubpluginArtifact(
+        groupId = ArtifactInfo.GROUP,
+        artifactId = getCompilerPluginId(),
+        version = ArtifactInfo.VERSION
+    )
+
+    override fun applyToCompilation(kotlinCompilation: KotlinCompilation<*>): Provider<List<SubpluginOption>> {
+        val project = kotlinCompilation.target.project
+        val extension = project.extensions.findByType(ExtraCarePluginExtension::class.java)
+            ?: ExtraCarePluginExtension()
+
+        // Add annotations as a dependency
+        project.dependencies.add(
+            "implementation",
+            "${ArtifactInfo.GROUP}:${ArtifactInfo.ANNOTATIONS_ARTIFACT}:${ArtifactInfo.VERSION}"
+        )
+
+        return project.provider {
+            listOf(
+                SubpluginOption(key = "enabled", value = extension.enabled.toString())
+            )
+        }
     }
 }
