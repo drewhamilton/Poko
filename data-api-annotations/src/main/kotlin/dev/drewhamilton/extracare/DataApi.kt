1db149e7d8b1167a917fdcc360837a65f4ebd391
Drew Hamilton <drew.hamilton.0@gmail.com>
2021-01-08 17:46:49 +0100

    Update annotation KDocs

--- data-api-annotations/src/main/kotlin/dev/drewhamilton/extracare/DataApi.kt
+++ data-api-annotations/src/main/kotlin/dev/drewhamilton/extracare/DataApi.kt
@@ -8,9 +8,11 @@ package dev.drewhamilton.extracare
  *
  * The generated functions will be based on class properties in the primary constructor. Class properties *not* in the
  * primary constructor will not be included, and primary constructor parameters that are not class properties will not
- * be included.
+ * be included. Compilation will fail if the annotated class does not include a primary constructor.
  *
  * Each function will only be generated if it is not already manually overridden in the annotated class.
+ *
+ * The annotated class cannot be a `data class`, an `inline class`, or an `inner class`.
  *
  * Like data classes, it is highly recommended that all properties used in equals/hashCode are immutable. `var`s,
  * mutable collections, and especially arrays should be avoided. The class itself should also be final. The compiler
