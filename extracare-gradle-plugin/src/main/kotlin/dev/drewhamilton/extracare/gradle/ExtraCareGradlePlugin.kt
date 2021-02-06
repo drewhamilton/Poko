10b4f02f764d8a522474c8653e0937378d148b04
Drew Hamilton <drew.hamilton.0@gmail.com>
2021-02-06 20:26:05 +0100

    Remove temp function

--- extracare-gradle-plugin/src/main/kotlin/dev/drewhamilton/extracare/gradle/ExtraCareGradlePlugin.kt
+++ extracare-gradle-plugin/src/main/kotlin/dev/drewhamilton/extracare/gradle/ExtraCareGradlePlugin.kt
@@ -29,8 +29,6 @@ class ExtraCareGradlePlugin : KotlinCompilerPluginSupportPlugin {
         version = ArtifactInfo.VERSION
     )
 
-    fun temp(): Nothing = TODO("Remove this")
-
     override fun applyToCompilation(kotlinCompilation: KotlinCompilation<*>): Provider<List<SubpluginOption>> {
         val project = kotlinCompilation.target.project
         val extension = project.extensions.getByType(ExtraCarePluginExtension::class.java)
