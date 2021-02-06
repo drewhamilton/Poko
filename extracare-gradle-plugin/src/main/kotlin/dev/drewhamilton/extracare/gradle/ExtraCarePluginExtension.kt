d401868e771771530890f8357b67348bd191c7a2
Drew Hamilton <drew.hamilton.0@gmail.com>
2021-02-06 15:48:04 +0100

    Add dataApiAnnotation property to Gradle plugin
    Only add default annotation dependency if no alternate annotation is specified

--- extracare-gradle-plugin/src/main/kotlin/dev/drewhamilton/extracare/gradle/ExtraCarePluginExtension.kt
+++ extracare-gradle-plugin/src/main/kotlin/dev/drewhamilton/extracare/gradle/ExtraCarePluginExtension.kt
@@ -4,9 +4,11 @@ import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
 import javax.inject.Inject
 
-abstract class ExtraCarePluginExtension @Inject constructor(
-    objects: ObjectFactory
-) {
-    var enabled: Property<Boolean> = objects.property(Boolean::class.javaObjectType)
+abstract class ExtraCarePluginExtension @Inject constructor(objects: ObjectFactory) {
+
+    val enabled: Property<Boolean> = objects.property(Boolean::class.javaObjectType)
         .convention(true)
+
+    val dataApiAnnotation: Property<String> = objects.property(String::class.java)
+        .convention(ExtraCareGradlePlugin.DEFAULT_DATA_API_ANNOTATION)
 }
