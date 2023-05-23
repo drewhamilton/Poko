f719172d42b4ac5826d414844e5a25e662822254
Drew Hamilton <github@drewhamilton.dev>
2023-05-23 16:48:43 -0700

    Add annotation for array content inspection

---
+++ poko-annotations/src/main/kotlin/dev/drewhamilton/poko/ExperimentalArrayContentSupport.kt
@@ -0,0 +1,7 @@
+package dev.drewhamilton.poko
+
+/**
+ * Denotes an annotation that enables support for array content reading, which is experimental and may change or break.
+ */
+@RequiresOptIn
+annotation class ExperimentalArrayContentSupport
