6cc3a929b1ef4e300192df806e92c7807352a643
Drew Hamilton <github@drewhamilton.dev>
2023-05-24 09:32:27 -0700

    Test all supported types of arrays

--- poko-compiler-plugin/src/test/resources/api/ArrayHolder.kt
+++ poko-compiler-plugin/src/test/resources/api/ArrayHolder.kt
@@ -7,9 +7,23 @@ import dev.drewhamilton.poko.ReadArrayContent
 @Suppress("Unused")
 @OptIn(ExperimentalArrayContentSupport::class)
 @Poko class ArrayHolder(
-    @ReadArrayContent val arrayReferenceType: Array<String>,
-    @ReadArrayContent val nullableArrayReferenceType: Array<String>?,
-    @ReadArrayContent val arrayPrimitiveType: IntArray,
-    @ReadArrayContent val nullableArrayPrimitiveType: IntArray?,
+    @ReadArrayContent val stringArray: Array<String>,
+    @ReadArrayContent val nullableStringArray: Array<String>?,
+    @ReadArrayContent val booleanArray: BooleanArray,
+    @ReadArrayContent val nullableBooleanArray: BooleanArray?,
+    @ReadArrayContent val byteArray: ByteArray,
+    @ReadArrayContent val nullableByteArray: ByteArray?,
+    @ReadArrayContent val charArray: CharArray,
+    @ReadArrayContent val nullableCharArray: CharArray?,
+    @ReadArrayContent val shortArray: ShortArray,
+    @ReadArrayContent val nullableShortArray: ShortArray?,
+    @ReadArrayContent val intArray: IntArray,
+    @ReadArrayContent val nullableIntArray: IntArray?,
+    @ReadArrayContent val longArray: LongArray,
+    @ReadArrayContent val nullableLongArray: LongArray?,
+    @ReadArrayContent val floatArray: FloatArray,
+    @ReadArrayContent val nullableFloatArray: FloatArray?,
+    @ReadArrayContent val doubleArray: DoubleArray,
+    @ReadArrayContent val nullableDoubleArray: DoubleArray?,
     // TODO: more
 )
