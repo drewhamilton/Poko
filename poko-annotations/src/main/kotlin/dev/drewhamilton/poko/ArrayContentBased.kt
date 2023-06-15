69a2f2a9cd3055b9258cf56e12d74e897eea0303
Drew Hamilton <github@drewhamilton.dev>
2023-06-15 12:07:14 -0700

    Update ArrayContentBased doc comments to include support for Any and generic properties

--- poko-annotations/src/main/kotlin/dev/drewhamilton/poko/ArrayContentBased.kt
+++ poko-annotations/src/main/kotlin/dev/drewhamilton/poko/ArrayContentBased.kt
@@ -5,13 +5,18 @@ package dev.drewhamilton.poko
  * content. This differs from the Poko class (and data class) default of comparing arrays by
  * reference only.
  *
- * Poko class properties of type [Array], [BooleanArray], [ByteArray], [CharArray], [ShortArray],
+ * Poko class properties of type [Array], [BooleanArray], [CharArray], [ByteArray], [ShortArray],
  * [IntArray], [LongArray], [FloatArray], and [DoubleArray] are supported, including nested
  * [Array] types.
  *
- * Properties of a generic type or of type [Any] that are assigned as arrays at runtime are not
- * currently supported. Properties of a value class type that wraps an array are not supported.
- * Tagging non-array properties with this annotation is an error.
+ * Properties of a generic type or of type [Any] are also supported. For these properties, Poko will
+ * generate a `when` statement that disambiguates the various array types at runtime and analyzes
+ * content if the property is an array. (Note that with this logic, typed arrays will never be
+ * considered equals to primitive arrays, even if they hold the same content. For example,
+ * `arrayOf(1, 2)` will not be considered equals to `intArrayOf(1, 2)`.)
+ *
+ * Properties of a value class type that wraps an array are not supported. Tagging non-array
+ * properties with this annotation is an error.
  *
  * Using array properties in data models is not generally recommended, because they are mutable.
  * Mutating an array marked with this annotation will cause the parent Poko class to produce
