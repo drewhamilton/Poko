5678bad421576c19d07cd51f3d56921b3028639d
Jake Wharton <jw@squareup.com>
2023-07-07 22:16:06 -0400

    Make the annotations a multiplatform project

--- poko-annotations/build.gradle.kts
+++ poko-annotations/build.gradle.kts
@@ -1,11 +1,37 @@
 import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
 
 plugins {
-  alias(libs.plugins.kotlin.jvm)
+  alias(libs.plugins.kotlin.multiplatform)
   alias(libs.plugins.dokka)
   alias(libs.plugins.mavenPublish)
 }
 
 kotlin {
+  jvm()
+
+  js().nodejs()
+
+  mingwX64()
+
+  linuxArm64()
+  linuxX64()
+
+  iosArm64()
+  iosSimulatorArm64()
+  iosX64()
+
+  macosArm64()
+  macosX64()
+
+  tvosArm64()
+  tvosX64()
+  tvosSimulatorArm64()
+
+  watchosArm32()
+  watchosArm64()
+  watchosDeviceArm64()
+  watchosSimulatorArm64()
+  watchosX64()
+
   explicitApi = ExplicitApiMode.Strict
 }
