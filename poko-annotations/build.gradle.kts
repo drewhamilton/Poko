43f805d41da8d75832f15149d66db262a53d3bb7
Drew Hamilton <github@drewhamilton.dev>
2023-08-31 11:09:23 -0700

    Add example K2 JS compile failure

--- poko-annotations/build.gradle.kts
+++ poko-annotations/build.gradle.kts
@@ -6,10 +6,23 @@ pokoBuild {
   publishing()
 }
 
+fun <T : org.jetbrains.kotlin.gradle.plugin.KotlinTarget> T.applyK2(): T {
+  compilations.configureEach {
+    compilerOptions.options.languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
+  }
+
+  // Dummy value required to disambiguate these targets' configurations.
+  // See https://kotlinlang.org/docs/multiplatform-set-up-targets.html#distinguish-several-targets-for-one-platform
+  attributes.attribute(Attribute.of("com.example.useK2", Boolean::class.javaObjectType), true)
+
+  return this
+}
+
 kotlin {
   jvm()
 
   js().nodejs()
+  js("jsK2").applyK2().nodejs()
 
   mingwX64()
 
---
+++ poko-annotations/src/commonMain/kotlin/dev/drewhamilton/poko/fail.kt
@@ -0,0 +1,5 @@
+package dev.drewhamilton.poko
+
+private val a = arrayOf(arrayOf("5%, 10%"), intArrayOf(5, 10), booleanArrayOf(false, true))
+private val b = arrayOf<Any>(arrayOf("5%, 10%"), intArrayOf(5, 10), booleanArrayOf(false, true))
+private val c = arrayOf("5%, 10%", 5, 10, false, true)
