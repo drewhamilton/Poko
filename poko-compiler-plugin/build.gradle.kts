4269cd975d50123cb6341fe88fdfdc3f3ef5a509
Drew Hamilton <github@drewhamilton.dev>
2023-10-31 15:36:05 -0700

    Use kotlin-compile-testing-fork (#253)
    and delete compiled KCT PR jar

--- poko-compiler-plugin/build.gradle.kts
+++ poko-compiler-plugin/build.gradle.kts
@@ -23,18 +23,7 @@ dependencies {
     testImplementation(libs.junit)
     testImplementation(libs.assertk)
     testImplementation(libs.testParameterInjector)
-    val kctVersion = libs.versions.kotlinCompileTesting.get()
-    if (kctVersion.endsWith("-local")) {
-        // Include the local KCT jar and its copied dependencies
-        val kctName = libs.kotlin.compileTesting.get().name
-        testImplementation(files("libs/$kctName-$kctVersion.jar"))
-        // Copied from KCT's build.gradle:
-        testImplementation("com.squareup.okio:okio:3.3.0")
-        testImplementation("io.github.classgraph:classgraph:4.8.158")
-        testImplementation("org.jetbrains.kotlin:kotlin-annotation-processing-embeddable:${libs.versions.kotlin.get()}")
-    } else {
-        testImplementation(libs.kotlin.compileTesting)
-    }
+    testImplementation(libs.kotlinCompileTestingFork)
 }
 
 tasks.withType<KotlinCompile>().configureEach {
