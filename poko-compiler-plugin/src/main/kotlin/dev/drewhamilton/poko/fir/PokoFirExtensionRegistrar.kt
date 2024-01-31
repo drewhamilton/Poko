46694328bf39ee06ceea51f82778df458c7c5a4c
Drew Hamilton <github@drewhamilton.dev>
2024-01-30 17:17:51 -0800

    First pass at implementing FIR error checking

---
+++ poko-compiler-plugin/src/main/kotlin/dev/drewhamilton/poko/fir/PokoFirExtensionRegistrar.kt
@@ -0,0 +1,13 @@
+package dev.drewhamilton.poko.fir
+
+import org.jetbrains.kotlin.fir.extensions.FirExtensionRegistrar
+import org.jetbrains.kotlin.name.ClassId
+
+internal class PokoFirExtensionRegistrar(
+    private val pokoAnnotation: ClassId,
+) : FirExtensionRegistrar() {
+    override fun ExtensionRegistrarContext.configurePlugin() {
+        +PokoFirExtensionSessionComponent.getFactory(pokoAnnotation)
+        +::PokoFirCheckersExtension
+    }
+}
