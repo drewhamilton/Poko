fc969e9298b4f18f6f2d584c7af4f968f2f41270
jamiesanson <jamie.sanson@mnscorp.net>
2024-09-13 10:56:20 +0100

    Add `java-test-fixtures` plugin to JVM sample

--- sample/jvm/build.gradle.kts
+++ sample/jvm/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
     id("dev.drewhamilton.poko")
+    `java-test-fixtures`
 }
 
 dependencies {
---
+++ sample/jvm/src/testFixtures/kotlin/dev/drewhamilton/poko/sample/jvm/SampleFixture.kt
@@ -0,0 +1,7 @@
+package dev.drewhamilton.poko.sample.jvm
+
+val SampleFixture = Sample(
+    int = 1,
+    requiredString = "String",
+    optionalString = null
+)
