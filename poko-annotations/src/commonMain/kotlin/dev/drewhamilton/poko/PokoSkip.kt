a405f2c1d6b07e77a25d9b88a1d8ec5e2823b21b
Drew Hamilton <github@drewhamilton.dev>
2024-11-18 11:38:23 -0600

    Introduce PokoSkip annotation

---
+++ poko-annotations/src/commonMain/kotlin/dev/drewhamilton/poko/PokoSkip.kt
@@ -0,0 +1,11 @@
+package dev.drewhamilton.poko
+
+/**
+ * Primary constructor properties marked with this annotation will be omitted from generated
+ * `equals`, `hashCode`, and `toString` functions, as if they were not properties.
+ *
+ * This annotation has no effect on properties declared outside of the primary constructor.
+ */
+@Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.PROPERTY)
+public annotation class PokoSkip
