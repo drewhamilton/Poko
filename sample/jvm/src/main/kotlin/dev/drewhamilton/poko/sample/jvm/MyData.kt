c643b91e8c44feb41d8f7844e2d9aaa04ba0a42e
Drew Hamilton <github@drewhamilton.dev>
2024-12-12 16:38:17 -0600

    Use custom annotation in sample:jvm module
    This ensures compilation works if no custom Skip annotation is provided.

---
+++ sample/jvm/src/main/kotlin/dev/drewhamilton/poko/sample/jvm/MyData.kt
@@ -0,0 +1,8 @@
+package dev.drewhamilton.poko.sample.jvm
+
+/**
+ * Local replacement for default Poko annotation.
+ */
+@Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.CLASS)
+annotation class MyData
