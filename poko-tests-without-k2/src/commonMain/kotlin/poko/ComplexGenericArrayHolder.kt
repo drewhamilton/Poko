add9db66c462ee22ef64f6d30b2b1dacd5a62f07
Drew Hamilton <github@drewhamilton.dev>
2024-12-13 13:29:17 -0600

    Replace project usages of ArrayContentBased with ReadArrayContent

--- poko-tests-without-k2/src/commonMain/kotlin/poko/ComplexGenericArrayHolder.kt
+++ poko-tests-without-k2/src/commonMain/kotlin/poko/ComplexGenericArrayHolder.kt
@@ -1,9 +1,8 @@
 package poko
 
-import dev.drewhamilton.poko.ArrayContentBased
 import dev.drewhamilton.poko.Poko
 
 @Suppress("Unused")
 @Poko class ComplexGenericArrayHolder<A : Any, G : A>(
-    @ArrayContentBased val generic: G,
+    @Poko.ReadArrayContent val generic: G,
 )
