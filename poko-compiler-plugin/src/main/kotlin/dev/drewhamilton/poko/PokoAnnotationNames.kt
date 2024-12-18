294e51af55221f0e6b393fc4693e2e6fd3857213
Drew Hamilton <github@drewhamilton.dev>
2024-12-18 10:09:31 -0600

    Add PokoAnnotationNames.Builder

--- poko-compiler-plugin/src/main/kotlin/dev/drewhamilton/poko/PokoAnnotationNames.kt
+++ poko-compiler-plugin/src/main/kotlin/dev/drewhamilton/poko/PokoAnnotationNames.kt
@@ -3,6 +3,7 @@ package dev.drewhamilton.poko
 import org.jetbrains.kotlin.name.Name
 
 internal object PokoAnnotationNames {
+    val Builder = Name.identifier("Builder")
     val ReadArrayContent = Name.identifier("ReadArrayContent")
     val Skip = Name.identifier("Skip")
 }
