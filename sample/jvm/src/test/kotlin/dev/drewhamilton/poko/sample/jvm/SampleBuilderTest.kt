118c4725ada1b5a84b789f238071a80dbee5214a
Drew Hamilton <github@drewhamilton.dev>
2024-12-18 16:57:08 -0600

    Generate builder setter IR body

--- sample/jvm/src/test/kotlin/dev/drewhamilton/poko/sample/jvm/SampleBuilderTest.kt
+++ sample/jvm/src/test/kotlin/dev/drewhamilton/poko/sample/jvm/SampleBuilderTest.kt
@@ -1,6 +1,7 @@
 package dev.drewhamilton.poko.sample.jvm
 
 import assertk.assertThat
+import assertk.assertions.isEqualTo
 import assertk.assertions.isNull
 import org.junit.Test
 
@@ -10,5 +11,15 @@ class SampleBuilderTest {
         assertThat<Int?>(builder.int).isNull()
         assertThat<String?>(builder.requiredString).isNull()
         assertThat<String?>(builder.optionalString).isNull()
+
+        builder
+            .setInt(1)
+            .setRequiredString("two")
+            .setOptionalString(null)
+            .setOptionalString("three")
+
+        assertThat<Int?>(builder.int).isEqualTo(1)
+        assertThat<String?>(builder.requiredString).isEqualTo("two")
+        assertThat<String?>(builder.optionalString).isEqualTo("three")
     }
 }
