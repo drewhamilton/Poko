95fd15d1007f4405854a60a5632d06a31e2ac659
Drew Hamilton <github@drewhamilton.dev>
2024-12-18 16:57:08 -0600

    Generate builder setter IR body

--- poko-compiler-plugin/src/main/kotlin/dev/drewhamilton/poko/ir/TransformerForBuilderGenerator.kt
+++ poko-compiler-plugin/src/main/kotlin/dev/drewhamilton/poko/ir/TransformerForBuilderGenerator.kt
@@ -10,19 +10,27 @@ import org.jetbrains.kotlin.GeneratedDeclarationKey
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrConstructor
+import org.jetbrains.kotlin.ir.declarations.IrFunction
 import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
 import org.jetbrains.kotlin.ir.declarations.createBlockBody
 import org.jetbrains.kotlin.ir.expressions.IrBody
 import org.jetbrains.kotlin.ir.expressions.impl.IrDelegatingConstructorCallImpl
+import org.jetbrains.kotlin.ir.expressions.impl.IrGetValueImpl
 import org.jetbrains.kotlin.ir.expressions.impl.IrInstanceInitializerCallImpl
+import org.jetbrains.kotlin.ir.expressions.impl.IrReturnImpl
+import org.jetbrains.kotlin.ir.expressions.impl.IrSetFieldImpl
 import org.jetbrains.kotlin.ir.symbols.UnsafeDuringIrConstructionAPI
 import org.jetbrains.kotlin.ir.types.IrSimpleType
+import org.jetbrains.kotlin.ir.types.classOrNull
+import org.jetbrains.kotlin.ir.util.parentAsClass
 import org.jetbrains.kotlin.ir.util.primaryConstructor
+import org.jetbrains.kotlin.ir.util.properties
 
 @OptIn(UnsafeDuringIrConstructionAPI::class)
 internal class TransformerForBuilderGenerator(
     context: IrPluginContext,
 ) : AbstractTransformerForGenerator(context, visitBodies = false) {
+
     override fun interestedIn(
         key: GeneratedDeclarationKey?,
     ): Boolean {
@@ -33,7 +41,61 @@ internal class TransformerForBuilderGenerator(
         function: IrSimpleFunction,
         key: GeneratedDeclarationKey?,
     ): IrBody? {
-        return null // TODO
+        return if (
+            function.extensionReceiverParameter == null &&
+            function.typeParameters.isEmpty() &&
+            function.valueParameters.size == 1 &&
+            function.returnType.classOrNull?.owner == function.parent
+        ) {
+            generateBuilderSetterFunctionBody(function)
+        } else {
+            // TODO
+            null
+        }
+    }
+
+    private fun generateBuilderSetterFunctionBody(
+        function: IrFunction,
+    ): IrBody {
+        val param = function.valueParameters.single()
+        val parentClassInstance = IrGetValueImpl(
+            startOffset = -1,
+            endOffset = -1,
+            type = function.dispatchReceiverParameter!!.type,
+            symbol = function.dispatchReceiverParameter!!.symbol,
+        )
+        val setFieldStatement = IrSetFieldImpl(
+            startOffset = -1,
+            endOffset = -1,
+            symbol = function.parentAsClass
+                .properties
+                .single { it.name == param.name }
+                .backingField!!
+                .symbol,
+            receiver = parentClassInstance,
+            value = IrGetValueImpl(
+                startOffset = -1,
+                endOffset = -1,
+                type = param.type,
+                symbol = param.symbol,
+            ),
+            type = irBuiltIns.unitType, // setter return type
+        )
+        val returnStatement = IrReturnImpl(
+            startOffset = -1,
+            endOffset = -1,
+            type = function.returnType,
+            returnTargetSymbol = function.symbol,
+            value = parentClassInstance,
+        )
+        return irFactory.createBlockBody(
+            startOffset = -1,
+            endOffset = -1,
+            statements = listOf(
+                setFieldStatement,
+                returnStatement,
+            ),
+        )
     }
 
     // Adapted from https://github.com/JetBrains/kotlin/blob/52a3ec9184fa44b2c8ce981f279cd66686dbe73b/plugins/plugin-sandbox/src/org/jetbrains/kotlin/plugin/sandbox/ir/AbstractTransformerForGenerator.kt#L89-L108
--- sample/jvm/src/test/kotlin/dev/drewhamilton/poko/sample/jvm/SampleBuilderTest.kt
+++ sample/jvm/src/test/kotlin/dev/drewhamilton/poko/sample/jvm/SampleBuilderTest.kt
@@ -1,14 +1,35 @@
 package dev.drewhamilton.poko.sample.jvm
 
 import assertk.assertThat
+import assertk.assertions.isEqualTo
 import assertk.assertions.isNull
 import org.junit.Test
 
 class SampleBuilderTest {
-    @Test fun `instantiate builder`() {
+    @Test fun `builder setters`() {
         val builder = Sample.Builder()
         assertThat<Int?>(builder.int).isNull()
         assertThat<String?>(builder.requiredString).isNull()
+        assertThat<String?>(builder.optionalString).isNull()
+
+        builder
+            .setInt(1)
+            .setRequiredString("two")
+            .setOptionalString(null)
+            .setOptionalString("three")
+
+        assertThat<Int?>(builder.int).isEqualTo(1)
+        assertThat<String?>(builder.requiredString).isEqualTo("two")
+        assertThat<String?>(builder.optionalString).isEqualTo("three")
+
+        builder.apply {
+            int = 4
+            requiredString = "five"
+            optionalString = null
+        }
+
+        assertThat<Int?>(builder.int).isEqualTo(4)
+        assertThat<String?>(builder.requiredString).isEqualTo("five")
         assertThat<String?>(builder.optionalString).isNull()
     }
 }
