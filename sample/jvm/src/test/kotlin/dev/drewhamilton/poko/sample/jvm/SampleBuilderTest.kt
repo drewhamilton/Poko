ed56866cd38cf43bb3ff22bf1a73fad8e4e48418
Drew Hamilton <github@drewhamilton.dev>
2024-12-18 10:55:30 -0600

    Add SampleBuilderTest to try out Builder in IDE

---
+++ sample/jvm/src/test/kotlin/dev/drewhamilton/poko/sample/jvm/SampleBuilderTest.kt
@@ -0,0 +1,9 @@
+package dev.drewhamilton.poko.sample.jvm
+
+import org.junit.Test
+
+class SampleBuilderTest {
+    @Test fun `instantiate builder`() {
+        val builder = Sample.Builder()
+    }
+}
