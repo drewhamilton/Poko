c1761842874b780c97ded3c3178282d1d5d9367c
Drew Hamilton <github@drewhamilton.dev>
2024-12-18 12:10:38 -0600

    Generate builder vars

--- sample/jvm/src/test/kotlin/dev/drewhamilton/poko/sample/jvm/SampleBuilderTest.kt
+++ sample/jvm/src/test/kotlin/dev/drewhamilton/poko/sample/jvm/SampleBuilderTest.kt
@@ -1,9 +1,14 @@
 package dev.drewhamilton.poko.sample.jvm
 
+import assertk.assertThat
+import assertk.assertions.isNull
 import org.junit.Test
 
 class SampleBuilderTest {
     @Test fun `instantiate builder`() {
         val builder = Sample.Builder()
+        assertThat<Int?>(builder.int).isNull()
+        assertThat<String?>(builder.requiredString).isNull()
+        assertThat<String?>(builder.optionalString).isNull()
     }
 }
