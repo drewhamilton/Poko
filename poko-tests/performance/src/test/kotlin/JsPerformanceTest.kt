10830632667733c14963e6d0c62b1ad92031ab42
Drew Hamilton <github@drewhamilton.dev>
2025-01-29 21:17:49 -0600

    Fix JsPerformanceTest
    Filter down to a single non-data class to check for THROW_CCE code

--- poko-tests/performance/src/test/kotlin/JsPerformanceTest.kt
+++ poko-tests/performance/src/test/kotlin/JsPerformanceTest.kt
@@ -1,15 +1,24 @@
-import assertk.all
+
+import assertk.assertAll
 import assertk.assertThat
-import assertk.assertions.contains
-import assertk.assertions.doesNotContain
+import assertk.assertions.hasSize
+import assertk.assertions.isEmpty
 import org.junit.Test
 
 class JsPerformanceTest {
     @Test fun `equals does not perform redundant instanceof check`() {
         val javascript = jsOutput().readText()
-        assertThat(javascript).all {
-            contains("other instanceof IntAndLong")
-            doesNotContain("THROW_CCE")
+
+        // Hack to filter out data classes, which do have the `THROW_CCE` code:
+        val intAndLongLines = javascript.split("\n").filter { it.contains("IntAndLong") }
+        assertAll {
+            assertThat(
+                actual = intAndLongLines.filter { it.contains("other instanceof IntAndLong") },
+            ).hasSize(1)
+
+            assertThat(
+                actual = intAndLongLines.filter { it.contains("THROW_CCE") },
+            ).isEmpty()
         }
     }
 }
