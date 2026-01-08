711149728aa45e8d7a440c4baea943a61ee5966a
Drew Hamilton <github@drewhamilton.dev>
2026-01-08 17:15:38 -0600

    Add explicit `isSynthetic` declaration to `PokoOrigin`
    This maintains compatibility with Kotlin 1.9.

--- poko-compiler-plugin/src/main/kotlin/dev/drewhamilton/poko/ir/PokoOrigin.kt
+++ poko-compiler-plugin/src/main/kotlin/dev/drewhamilton/poko/ir/PokoOrigin.kt
@@ -7,6 +7,10 @@ import org.jetbrains.kotlin.ir.declarations.IrDeclarationOrigin
 internal object PokoOrigin : IrDeclarationOrigin, ReadOnlyProperty<Any?, PokoOrigin> {
     override val name: String = "GENERATED_POKO_CLASS_MEMBER"
 
+    // TODO: Remove when support for Kotlin 1.9 is dropped
+    override val isSynthetic: Boolean
+        get() = false
+
     override fun toString(): String = name
 
     override fun getValue(thisRef: Any?, property: KProperty<*>): PokoOrigin = this
